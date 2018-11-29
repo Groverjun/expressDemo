a1eda80b8c92a0f99c7d32851ff41d0238cdb370
Groverjun <1250901900@qq.com>
2018-11-29 18:34:50 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var mysql  = require('mysql'); 
+
+//创建一个connection
+var connection = mysql.createConnection({     
+  host     : 'localhost',       //主机
+  user     : 'root',               //MySQL认证用户名
+  password : '123456',        //MySQL认证用户密码
+  port: '3308',     //端口号
+  database: 'nodetest', //数据库名
+  
+});
+
+
+/**sql语句 **/
+
+var  userGetSql = 'SELECT * FROM userinfo '; //查询
+
+/**sql语句结束*/
+function query(res){
+		connection.connect();
+	  var res=res
+		connection.query(userGetSql,function (err, result) {
+		    if(err){return err.message;}   
+		    console.log(result)
+		  	res.send(result);
+		});
+		connection.end();		
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 router.get('/first', function(req, res, next) {
-	console.log(req.query)
-	let data=req.query
-	if(data.name=='冬马和纱'){
-		res.send({name:'冬马和纱',say:'你好'});
-	}else if(data.name=='小木曾雪莱'){
-		res.send({name:'小木曾雪莱',say:'再见'});
-	}else{
-		res.send({name:'???',say:'不认识'});
-	}
-	
-	
+	query(res)
 });
+
 module.exports = router;
